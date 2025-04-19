860f506a538f656f48cdf2920c07c7d59d990d6e
usamaru33 <raioraioraio15@gmail.com>
2025-04-19 14:59:44 +0900

    シーンリセット機能実装

---
+++ Unity/Assets/Scripts/ResetScene.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene@: MonoBehaviour
+{
+    private float pressTime = 0f;
+    private const float REQUIRED_PRESS_TIME = 3f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            pressTime += Time.deltaTime;
+
+            if (pressTime >= REQUIRED_PRESS_TIME)
+            {
+                SceneManager.LoadScene("WaitingScene");
+            }
+        }
+        else
+        {
+            pressTime = 0f;
+        }
+    }
+}

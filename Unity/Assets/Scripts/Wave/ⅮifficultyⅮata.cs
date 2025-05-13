73886dbce0e0347ccdada75041d8278e3ba74fec
usamaru33 <raioraioraio15@gmail.com>
2025-05-14 02:12:51 +0900

    hardモード実装

---
+++ Unity/Assets/Scripts/Wave/ⅮifficultyⅮata.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DifficultyData", menuName = "ScriptableObjects/DifficultyDataSO", order = 1)]
+public class  DifficultyDataSO : ScriptableObject
+{
+    public bool rotationFlag;
+}
